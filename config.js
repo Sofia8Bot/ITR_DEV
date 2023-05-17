2be5ed9a5e73458ef3e9d665351b6540f4f5a7a3
Sofia8Bot <129605742+Sofia8Bot@users.noreply.github.com>
2023-05-17 17:16:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,20 +2,20 @@ const toBool = (x) => x == 'true'
 const { existsSync } = require('fs')
 if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', //your ssid to run bot
+    SESSION_ID: process.env.SESSION_ID || 'inrl~0b628Y5Dzd857a62bac755c8dd1f44072c8b', //your ssid to run bot
     MONGO_URL : process.env.MONGO_URL,//must be enter your mongo url;
     HEROKU: {
         API_KEY: process.env.HEROKU_API_KEY,
         APP_NAME: process.env.HEROKU_APP_NAME
     },
     BASE_URL : "https://inrl-web.onrender.com/",
-    TUTORIAL : "https://youtube.com/@noureddineouafy2",
+    TUTORIAL : "https://youtube.com",
     PMB_MSG : "pm msgs isn't allowed",
     PMBC_MSG : "pm call isn't allowed",
     AUTOMUTE_MSG : "_group will been muted at @time_",
     AUTOUNMUTE_MSG : "_group will unmute at @time_",
-    GIT : "www.instagram.com/noureddine_ouafy",
-    WAGRP : 'www.instagram.com/noureddine_ouafy',
+    GIT : "www.instagram.com",
+    WAGRP : 'www.instagram.com',
     ALLWAYS_ONLINE: toBool(process.env.ALLWAYS_ONLINE || "true"),
     REACT : toBool(process.env.REACT || "false"),
     AUTO_BIO : toBool(process.env.AUTO_BIO || "false"),
@@ -26,21 +26,21 @@ module.exports = {
     READ_CHAT : toBool(process.env.READ_CHAT ||  "false"),
     AUTO_CHAT_PM : toBool(process.env.AUTO_CHAT_PM || "false"),
     AUTO_CHAT_GRP : toBool(process.env.AUTO_CHAT_GRP || "false"),
-    FOOTER : process.env.FOOTER || "zaytouna-ᴍᴅ",
+    FOOTER : process.env.FOOTER || "SoFia-Bot",
     PASSWORD : process.env.PASSWORD || 'inrl-bot~md',
     WARNCOUND : process.env.WARNCOUND || 5,
     ALIVE_DATA : process.env.ALIVE_DATA || "$text>_iam alive now &sender_",
-    BOT_INFO : process.env.BOT_INFO || "212605784394;INRL-BOT-MD;INRL;https://telegra.ph/file/c6ba7236049356210a8b5.jpg",
+    BOT_INFO : process.env.BOT_INFO || "201158862316;SoFia-BOT-MD;INRL;https://telegra.ph/file/bf05de7dd345556d86853.jpg",
     WORKTYPE : process.env.WORKTYPE || "public",
-    PREFIX : process.env.PREFIX || false,
+    PREFIX : process.env.PREFIX || true,
     WELCOME_MSG : process.env.WELCOME_MSG || "$text>_hey bro/sis_ *&user*\nthanks for join;$image>&pp;",
     EXIT_MSG : process.env.EXIT_MSG || "$text>_goodbye _ *&user*;$image>&pp;",
     LANG : process.env.LANG || "en",
     BLOCK_CHAT : process.env.BLOCK_CHAT || "jid@g.us, jid2@g.us",//set chat similarly
     BOT_PRESENCE : process.env.BOT_PRESENCE || "recording",
-    AUDIO_DATA : process.env.AUDIO_DATA || "ᴍᴜꜱɪᴄ;ᴋɪᴅ;https://telegra.ph/file/c6ba7236049356210a8b5.jpg",
-    STICKER_DATA : process.env.AUDIO_DATA || "zaytouna;bot",
+    AUDIO_DATA : process.env.AUDIO_DATA || "ᴍᴜꜱɪᴄ;ᴋɪᴅ;https://https://telegra.ph/file/bf05de7dd345556d86853.jpg",
+    STICKER_DATA : process.env.AUDIO_DATA || "SoFia;bot",
     INSTAGRAM :  process.env.INSTAGRAM || "nullX",
-    CAPTION : process.env.CAPTION || "created by www.instagram.com/noureddine_ouafy",
-    SUDO : process.env.SUDO || "212605784394"
+    CAPTION : process.env.CAPTION || "created by www.instagram.com",
+    SUDO : process.env.SUDO || "2011588862316"
 };
